7fae06418c483ab3c3a879ccc0afe0c3f83eddb8
Jonas Eana <jonas@eana.ro>
2021-01-29 20:16:56 +0100

    Add aws-nat-instance

---
+++ modules/aws-nat-instance/data.tf
@@ -0,0 +1,28 @@
+## DATA
+## ----------------------------------------------------------------------------
+
+# AMI of the latest Amazon Linux 2
+data "aws_ami" "this" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "block-device-mapping.volume-type"
+    values = ["gp2"]
+  }
+}
