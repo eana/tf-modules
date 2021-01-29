925580ed9b87ef0a484f67051ff1fe742314edc1
Jonas Eana <jonas@eana.ro>
2021-01-29 23:18:40 +0100

    Fix cidr_blocks

--- modules/aws-nat-instance/main.tf
+++ modules/aws-nat-instance/main.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "this" {
 resource "aws_security_group_rule" "egress" {
   security_group_id = aws_security_group.this.id
   type              = "egress"
-  cidr_blocks       = "0.0.0.0/0" # tfsec:ignore:AWS007
+  cidr_blocks       = ["0.0.0.0/0"] # tfsec:ignore:AWS007
   from_port         = 0
   to_port           = 65535
   protocol          = "tcp"
